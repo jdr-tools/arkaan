c60f497d2762cbf5e0a57f5333d6c079636674d5
babausse <courtois.vincent@outlook.com>
2017-11-26 14:05:31 +0100

    Add missing documentation

--- lib/arkaan/authentication.rb
+++ lib/arkaan/authentication.rb
@@ -1,4 +1,6 @@
 module Arkaan
+  # This module holds the logic for user authentication to our frontend.
+  # @author Vincent Courtois <courtois.vincent@outlook.com>
   module Authentication
     autoload :Session, 'arkaan/authentication/session'
   end
--- lib/arkaan/authentication/session.rb
+++ lib/arkaan/authentication/session.rb
@@ -1,13 +1,22 @@
 module Arkaan
   module Authentication
+    # A session represents the connection of the user on our frontend application. Nobody else than our frontend should
+    # have access to the session or it's content (in particular to the token), instead they shall use the OAuth2.0 protocol.
+    # A session shall ONLY be created by a premium application (only our frontend applications are premium).
+    # @author Vincent Courtois <courtois.vincent@outlook.com>
     class Session
       include Mongoid::Document
       include Mongoid::Timestamps
 
+      # @!attribute [rw] token
+      #   @return [String] the unique token for this session, used to identify it and be sure the user is connected on this application.
       field :token, type: String
-
+      # @!attribute [rw] expiration
+      #   @return [Integer] the time of expiration of the session, in seconds.
       field :expiration, type: Integer, default: 0
 
+      # @!attribute [rw] account
+      #   @return [Arkaan::Account] the account connected to the application.
       belongs_to :account, class_name: 'Arkaan::Account', inverse_of: :sessions
 
       validates :token,
