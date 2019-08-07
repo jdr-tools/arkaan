a715cfa14a3245cb0b116a2ef018cd629d4d8f5d
babausse <courtois.vincent@outlook.com>
2019-08-07 01:06:35 -0700

    Repair broken specs

--- spec/factories/monitoring/vigilantes_factory.rb
+++ spec/factories/monitoring/vigilantes_factory.rb
@@ -2,9 +2,6 @@ FactoryGirl.define do
   factory :empty_vigilante, class: Arkaan::Monitoring::Vigilante do
     factory :vigilante do
       token 'vigilante_token'
-      key 'vigilante'
-      path '/vigilante'
-      diagnostic '/status'
     end
   end
 end
