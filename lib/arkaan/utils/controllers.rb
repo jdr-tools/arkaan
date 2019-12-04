0038b5c92dfda6187109f98aa843406472848e33
Vincent <courtois.vincent@outlook.com>
2019-12-04 13:54:24 +0100

    Clean the utility classes that are now in the Virtuatable gem

--- lib/arkaan/utils/controllers.rb
+++
@@ -1,8 +0,0 @@
-module Arkaan
-  module Utils
-    module Controllers
-      autoload :Base   , 'arkaan/utils/controllers/base'
-      autoload :Checked, 'arkaan/utils/controllers/checked'
-    end
-  end
-end
