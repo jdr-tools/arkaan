4289a857df2be40ab5c0d7aa173652c0990f7d63
babausse <courtois.vincent@outlook.com>
2020-10-26 23:37:29 +0100

    Change the repository from which search the labels

---
+++ integration/version.rb
@@ -0,0 +1,42 @@
+require 'gems'
+require 'semantic'
+require 'gitlab'
+
+Gitlab.endpoint = 'https://gitlab.com/api/v4'
+Gitlab.private_token = ENV['GITLAB_API_TOKEN']
+
+def last_commit
+  commits = Gitlab.commits(
+    'virtuatable/services/arkaan',
+    { page: 1, per_page: 1 }
+  )
+  commits.first
+end
+
+def current_version
+  Semantic::Version.new(Gems.info('arkaan')['version'])
+end
+
+def next_version
+  current_version.increment!(increment)
+end
+
+def increment
+  %w[major minor].each do |label|
+    return label if labels.include? label
+  end
+  'patch'
+end
+
+def labels
+  merge_request = Gitlab.merge_requests(ENV['CI_PROJECT_ID']).find do |mr|
+    mr['merge_commit_sha'] == last_commit['id']
+  end
+  merge_request.nil? ? [] : merge_request['labels']
+end
+
+if ARGV.first == 'next'
+  puts next_version.to_s
+else
+  puts current_version.to_s
+end
